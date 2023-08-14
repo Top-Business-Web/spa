25651ecb7438a0336d0637a3bb7e464332a8b7d6
Abdullah <abdullah.alhumsi9@gmail.com>
2023-08-14 11:59:47 +0300

    add vendor and fix A to a 6

--- resources/views/admin/admin/index.blade.php
+++ resources/views/admin/admin/index.blade.php
@@ -82,7 +82,7 @@
         </div>
         <!-- Create Or Edit Modal -->
     </div>
-    @include('Admin/layouts/myAjaxHelper')
+    @include('admin/layouts/myAjaxHelper')
 @endsection
 @section('ajaxCalls')
     <script>
